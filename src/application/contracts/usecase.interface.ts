c6fe2e4f709c8371ef0ebeecdf804a48eaa40e8f
Yury Tomaz <contato@viladev.com>
2024-01-24 17:47:17 -0300

    feat(init-baileys): implement InitBailesInstanceUseCase
    - Defines InitBaileysInstanceUseCaseInputDTO, InitBailesInstanceUseCaseOutputDTO, and InitBailesInstanceUseCaseInterface
    - Implements InitBailesInstanceUseCase to handle the initialization of Baileys instances
    - Executes the use case based on the provided input parameters
    - Returns the output containing key, webhook, allowWebhook, heardEvents, and isOn status

---
+++ src/application/contracts/usecase.interface.ts
@@ -0,0 +1,22 @@
+export interface InitBaileysInstanceUseCaseInputDTO {
+    key: string;
+    webhook?: string;
+    allowWebhook?: boolean;
+    heardEvents: string[];
+    isWebhookBase64?: boolean;
+    markMessagesRead?: boolean;
+    apiKey?: string;
+}
+
+export interface InitBailesInstanceUseCaseOutputDTO {
+    key: string;
+    webhook: string | undefined;
+    allowWebhook: boolean;
+    heardEvents: string[];
+    isOn: boolean;
+
+}
+
+export interface InitBailesInstanceUseCaseInterface {
+    execute(input: InitBaileysInstanceUseCaseInputDTO): Promise<InitBailesInstanceUseCaseOutputDTO>;
+}
