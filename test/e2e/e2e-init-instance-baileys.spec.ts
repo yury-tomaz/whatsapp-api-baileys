d321bdeb2c1a71d26f49d4b52de0e11ea6701c7b
Yury Tomaz <contato@viladev.com>
2024-01-24 17:59:06 -0300

    test(e2e): implement Init Baileys Instance end-to-end test
    - Sets up an end-to-end test suite for initializing Baileys instances via API
    - Uses supertest to perform HTTP requests against the server
    - Defines a test case for starting a Baileys instance using valid input parameters
    - Generates mock data for the Baileys initialization request using faker
    - Verifies that the API responds with a status code of 200 for a successful initialization

---
+++ test/e2e/e2e-init-instance-baileys.spec.ts
@@ -0,0 +1,39 @@
+import request from 'supertest';
+import { IncomingMessage, Server, ServerResponse } from 'http';
+import { app, client } from '../../src/main/server/app'
+import { logger } from '../../src/infrastructure/logger';
+import TestAgent from 'supertest/lib/agent';
+import { faker } from '@faker-js/faker';
+
+describe('init Baileys instance e2e test', () => {
+    let server: Server<typeof IncomingMessage, typeof ServerResponse>;
+    let api: TestAgent;
+
+
+    beforeAll(async () => {
+        server = app.listen(() => logger.info('Server is running on port'));
+        api = request(server);
+    });
+
+    afterAll(async () => {
+        client.close();
+        server.close();
+    });
+
+    it('should be possible to start an instance of Baileys', async () => {
+
+        const data = {
+            key: faker.string.uuid(),
+            webhook: faker.internet.url(),
+            allowWebhook: false,
+            heardEvents: ['all'],
+            isWebhookBase64: false,
+            markMessagesRead: false,
+            apiKey: faker.string.uuid()
+        }
+
+        const response = await api.post('/api/v1/baileys').send(data);
+
+        expect(response.status).toBe(200);
+    });
+});
