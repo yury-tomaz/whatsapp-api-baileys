30a95fa8344472313d785fd017a7402a13646108
Yury Tomaz <contato@viladev.com>
2024-01-25 21:48:58 -0300

    refactor(api): reorganize Baileys routes and add versioning middleware
    - Move Baileys routes to '/api/baileys'
    - Introduce versioning middleware for route handling
    - Add endpoint for sending text messages

--- src/main/routes/index.ts
+++ src/main/routes/index.ts
@@ -1,11 +1,27 @@
 import { Router } from "express";
-import { requestAdapter } from "../adapters/request-adapter";
 import { initBaileysInstanceComposer } from "../composers/baileys/Init-baileys-instance.composer";
 import { GetQrCodeComposer } from "../composers/baileys/get-QrCode.composer";
+import { sendTextMessageCompose } from "../composers/baileys/send-text-message.composer";
+import { routesVersioning } from "../middlewares/route-versioning.middleware";
 
 const router = Router();
 
-router.post("/api/v1/baileys", (request, response) => requestAdapter(request, response,  initBaileysInstanceComposer()));
-router.get("/api/v1/baileys/:key/QRCode", (request, response) => requestAdapter(request, response,  GetQrCodeComposer()));
+router.post("/api/baileys", (request, response) =>
+    routesVersioning(request, response, {
+        '1.0.0': initBaileysInstanceComposer()
+    })
+);
+
+router.get("/api/baileys/:key/QRCode", (request, response) =>
+    routesVersioning(request, response, {
+        '1.0.0': GetQrCodeComposer()
+    })
+);
+
+router.post("/api/baileys/:key/message/text", (request, response) => 
+    routesVersioning(request, response, {
+        '1.0.0': sendTextMessageCompose()
+    })
+);
 
 export { router };
