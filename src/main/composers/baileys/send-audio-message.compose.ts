376574e8dff795292193c4308dc4519c7cefa0b5
Yury Tomaz <contato@viladev.com>
2024-01-27 12:51:23 -0300

    fix: correct the import of 'sendAudioMessageUseCase'

--- src/main/composers/baileys/send-audio-message.compose.ts
+++ src/main/composers/baileys/send-audio-message.compose.ts
@@ -1,10 +1,11 @@
-import { sendAudioMessageUseCase } from "../../../application/features/commands/baileys/send-audio-message/send-audio-message.usecase";
+
+import { SendAudioMessageUseCase } from "../../../application/features/commands/baileys/send-audio-message/send-audio-message.usecase";
 import { BaileysInMemoryRepository } from "../../../infrastructure/persistence/repositories/baileys-in-memory.repository";
 import { SendAudioMessageController } from "../../../presentation/controllers/baileys/commands/send-audio-message.controller";
 
 export const sendAudioMessageCompose = () => {
     const repositoryInMemory = BaileysInMemoryRepository.getInstance();
-    const useCase = new sendAudioMessageUseCase(repositoryInMemory);
+    const useCase = new SendAudioMessageUseCase(repositoryInMemory);
     const controller = new SendAudioMessageController(useCase);
 
     return controller;
