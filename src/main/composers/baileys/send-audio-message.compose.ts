3b0d80e23f8fe21b690f1e609231d1a102292231
Yury Tomaz <contato@viladev.com>
2024-01-26 21:31:17 -0300

    feat: implement sendAudioMessageCompose function

---
+++ src/main/composers/baileys/send-audio-message.compose.ts
@@ -0,0 +1,11 @@
+import { sendAudioMessageUseCase } from "../../../application/features/commands/baileys/send-audio-message/send-audio-message.usecase";
+import { BaileysInMemoryRepository } from "../../../infrastructure/persistence/repositories/baileys-in-memory.repository";
+import { SendAudioMessageController } from "../../../presentation/controllers/baileys/commands/send-audio-message.controller";
+
+export const sendAudioMessageCompose = () => {
+    const repositoryInMemory = BaileysInMemoryRepository.getInstance();
+    const useCase = new sendAudioMessageUseCase(repositoryInMemory);
+    const controller = new SendAudioMessageController(useCase);
+
+    return controller;
+};
