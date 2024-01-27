fe8efd5c03499af2aaf279d769e34876a016eb77
Yury Tomaz <contato@viladev.com>
2024-01-27 17:19:09 -0300

    feat: add QRCode rendering route for development environment

--- src/main/routes/baileys.routes.ts
+++ src/main/routes/baileys.routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { routesVersioning } from "../middlewares/route-versioning.middleware";
 import { initBaileysInstanceComposer } from "../composers/baileys/Init-baileys-instance.composer";
 import { GetQrCodeComposer } from "../composers/baileys/get-QrCode.composer";
+import { requestAdapter } from "../adapters/request-adapter";
+import environment from "../../infrastructure/environment";
 
 const baileysRoute = Router();
 
@@ -16,5 +18,9 @@ baileysRoute.get("/:key/QRCode", (request, response) =>
         '1.0.0': GetQrCodeComposer()
     })
 );
+
+if (environment.NODE_ENV === 'development') {
+    baileysRoute.get("/:key/QRCode/render", (request, response) => requestAdapter(request, response, GetQrCodeComposer(), 'index'));
+}
 
 export default baileysRoute as Router;
