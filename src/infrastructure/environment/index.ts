876b46644be6cef2e7504c8588d0d4abb2217e36
Yury Tomaz <contato@viladev.com>
2024-01-27 17:09:47 -0300

    feat: add environment variable and update exports

--- src/infrastructure/environment/index.ts
+++ src/infrastructure/environment/index.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+const NODE_ENV = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017';
 const MONGO_DB = process.env.MONGO_DB || 'whiskey';
@@ -5,5 +7,6 @@ const MONGO_DB = process.env.MONGO_DB || 'whiskey';
 export default {
     PORT,
     MONGO_URL,
-    MONGO_DB
+    MONGO_DB,
+    NODE_ENV
 }
