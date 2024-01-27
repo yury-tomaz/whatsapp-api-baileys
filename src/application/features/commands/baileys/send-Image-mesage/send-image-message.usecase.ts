e386e0feb11e695af5895b3284580b30312b8c4c
Yury Tomaz <contato@viladev.com>
2024-01-26 21:28:30 -0300

    feat: implement SendImageMessageUseCase  class
    implemented this use case to handle send image message

---
+++ src/application/features/commands/baileys/send-Image-mesage/send-image-message.usecase.ts
@@ -0,0 +1,45 @@
+import { AppError } from "../../../../../domain/exceptions/app-error";
+import { BaileysInstanceRepositoryInterface } from "../../../../../domain/repositories/baileys-instance.repository.interface";
+import { SendImageMessageUseCaseInterface } from "../../../../contracts/send-image-message-usecase.interface";
+import { SendImageMessageUseCaseInputDTO } from "./send-image-message.dto";
+
+export class SendImageMessageUseCase implements SendImageMessageUseCaseInterface {
+    constructor(
+        private readonly baileysInstanceRepository: BaileysInstanceRepositoryInterface
+    ) { }
+
+    async execute(input: SendImageMessageUseCaseInputDTO) {
+        const result = await this.baileysInstanceRepository.find(input.key);
+
+        if (!result) {
+            throw new AppError({
+                message: 'Baileys instance not found',
+                statusCode: 204,
+                isOperational: true
+            });
+        }
+
+        if (!result.waSocket) {
+            throw new AppError({
+                message: 'Baileys instance not initialized',
+                statusCode: 204,
+                isOperational: true
+            });
+        }
+
+        const sock = result.waSocket;
+        await result.verifyId(this.getWhatsAppId(input.to));
+
+        await sock.sendMessage( this.getWhatsAppId(input.to), {
+            mimetype: input.file.mimetype,
+            image: input.file.buffer,
+            caption: input.caption?? '',
+            fileName: input.file.originalname
+        });
+    }
+
+    private getWhatsAppId(id: string) {
+        if (id.includes('@g.us') || id.includes('@s.whatsapp.net')) return id
+        return id.includes('-') ? `${id}@g.us` : `${id}@s.whatsapp.net`
+    }
+}
