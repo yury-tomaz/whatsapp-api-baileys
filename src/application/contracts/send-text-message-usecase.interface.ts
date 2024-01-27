9733b5163f396780dcf33d86cded959a037ffb67
Yury Tomaz <contato@viladev.com>
2024-01-27 17:35:34 -0300

    fix: update import path for SendTextMessageUseCaseInputDTO

--- src/application/contracts/send-text-message-usecase.interface.ts
+++ src/application/contracts/send-text-message-usecase.interface.ts
@@ -1,4 +1,4 @@
-import { SendTextMessageUseCaseInputDTO } from "../features/commands/baileys/send-text-message/send-text-message.usecase";
+import { SendTextMessageUseCaseInputDTO } from "../features/commands/baileys/send-text-message/send-text-message.dto";
 
 export interface SendTextMessageUseCaseInterface {
     execute(input: SendTextMessageUseCaseInputDTO): Promise<void>;
