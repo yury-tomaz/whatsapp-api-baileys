57d44ef8ee47f54c613930a3e71627cdfb77b5ce
Yury Tomaz <contato@viladev.com>
2024-01-27 17:10:45 -0300

    refactor: update baileys route path

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import messageRouter from "./message.routes";
 import baileysRoute from "./baileys.routes";
-
 const router = Router();
 
-router.use('/api/baileys', baileysRoute);
+router.use('/api/baileys/instances', baileysRoute);
 router.use('/api/baileys/:key/message', messageRouter);
 
 export { router };
