26f37457eb71ab456e235795b80ffe7733b79b92
Yury Tomaz <contato@viladev.com>
2024-01-27 17:10:18 -0300

    refactor: requestAdapter function to support rendering

--- src/main/adapters/request-adapter.ts
+++ src/main/adapters/request-adapter.ts
@@ -2,11 +2,16 @@ import { Request as ExpressRequest, Response as ExpressResponse, NextFunction } 
 import { HttpRequest } from '../../presentation/http-types/http-request';
 import { HttpResponse } from '../../presentation/http-types/http-response';
 
-export interface ControllerInterface{
+export interface ControllerInterface {
     handle(request: HttpRequest): Promise<HttpResponse>;
 }
 
-export async function requestAdapter(request: ExpressRequest, response: ExpressResponse, controller: ControllerInterface): Promise<void> {
+export async function requestAdapter(
+    request: ExpressRequest,
+    response: ExpressResponse,
+    controller: ControllerInterface,
+    render?: string
+): Promise<void> {
     let body: any = null;
     if (request.body) {
         body = request.body;
@@ -23,7 +28,10 @@ export async function requestAdapter(request: ExpressRequest, response: ExpressR
     });
 
     const http_response = await controller.handle(http_request);
-    
+
+    if (render) {
+        return response.render(render, http_response.body);
+    }
 
     response.status(http_response.statusCode).json(http_response.body);
 }
