b79010660cc420084c10c1234fbe7d9e50c97f93
yury-tomaz <contato@viladev.com>
2024-04-28 17:30:38 -0300

    refactor: implement getWhatsAppId helper

---
+++ src/modules/@shared/infra/services/whatsapp/bailyes/helpers/get-whats-app-Id.ts
@@ -0,0 +1,5 @@
+
+export const getWhatsAppId = (id: string) => {
+    if (id.includes('@g.us') || id.includes('@s.whatsapp.net')) return id
+    return id.includes('-') ? `${id}@g.us` : `${id}@s.whatsapp.net`
+}
