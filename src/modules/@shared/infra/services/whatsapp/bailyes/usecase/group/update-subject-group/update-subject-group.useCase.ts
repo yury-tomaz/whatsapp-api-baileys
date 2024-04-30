9fbb2281597159f4edbce3bc89815c3e5aceddd2
Gabriel-souzaa <gabrielfreitassouzaa@gmail.com>
2024-04-29 22:57:28 -0300

    feat: to add updante description and subjet group

---
+++ src/modules/@shared/infra/services/whatsapp/bailyes/usecase/group/update-subject-group/update-subject-group.useCase.ts
@@ -0,0 +1,28 @@
+import {BaileysManager} from "../../../baileys-manager";
+import {getWhatsAppId} from "../../../helpers/get-whats-app-Id";
+import {checkInstance} from "../../../validate/check-Instance";
+import { UpdateSubjectGroupDto } from "./update-subject-group.dto";
+
+
+export class UpdateSubjectGroupUseCase {
+    constructor(
+        private baileysManager: BaileysManager
+    ) {
+    }
+
+    async execute(input: UpdateSubjectGroupDto) {
+        const result = await checkInstance(input.id, this.baileysManager)
+
+        const sock = result.waSocket!
+        
+        const whatsappId = getWhatsAppId(input.groupId);
+        await result.verifyId(whatsappId);
+
+        const group =  await sock.groupUpdateSubject(
+          whatsappId,
+          input.subject
+        )
+
+        return group;
+    }
+}
