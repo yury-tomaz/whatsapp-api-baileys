55ad0d5200fac38f6f1f0235bd331b929ca2e9e3
Gabriel-souzaa <gabrielfreitassouzaa@gmail.com>
2024-04-29 23:05:17 -0300

    feat: to add invite code in group

---
+++ src/modules/@shared/infra/services/whatsapp/bailyes/usecase/group/invite-code-group/invite-code-group.useCase.ts
@@ -0,0 +1,25 @@
+import {BaileysManager} from "../../../baileys-manager";
+import {getWhatsAppId} from "../../../helpers/get-whats-app-Id";
+import {checkInstance} from "../../../validate/check-Instance";
+import { InviteCodeGroupDto } from "./invite-code-group.dto";
+
+
+export class InviteCodeGroupUseCase {
+    constructor(
+        private baileysManager: BaileysManager
+    ) {
+    }
+
+    async execute(input: InviteCodeGroupDto) {
+        const result = await checkInstance(input.id, this.baileysManager)
+
+        const sock = result.waSocket!
+        
+        const whatsappId = getWhatsAppId(input.groupId);
+        await result.verifyId(whatsappId);
+
+        const invite =  await sock.groupInviteCode(whatsappId);
+
+        return invite;
+    }
+}
