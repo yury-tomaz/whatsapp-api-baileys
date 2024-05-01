163db59cb88fded449ac0aac7c7aa2da74fcb142
yury-tomaz <contato@viladev.com>
2024-05-01 15:16:54 -0300

    feat: update Baileys model

--- src/modules/@shared/infra/persistence/entity-configurations/baileys-model.ts
+++ src/modules/@shared/infra/persistence/entity-configurations/baileys-model.ts
@@ -1,15 +1,8 @@
 import { ObjectId } from "mongodb";
 
-// TODO: update model
 export default class Baileys {
     constructor(
-        public key: string,
-        public webhook: string,
-        public allowWebhook: boolean,
-        public heardEvents: string[],
-        public markMessagesRead: boolean,
-        public isWebhookBase64: boolean,
-        public apiKey?: string,
+        public tile: string,
         public createdAt?: Date,
         public updatedAt?: Date,
         public _id?: ObjectId,
