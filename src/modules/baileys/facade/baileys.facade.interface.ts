dbbe6aeb808e417825e3660340e33d1e224c6aab
yury-tomaz <contato@viladev.com>
2024-05-02 20:05:38 -0300

    fix: fix import of DTOs

--- src/modules/baileys/facade/baileys.facade.interface.ts
+++ src/modules/baileys/facade/baileys.facade.interface.ts
@@ -1,14 +1,17 @@
-import {InitInstanceDto} from "../usecase/instance/init/init-instance.dto";
 import {GetInfoUseCaseDTO} from "../usecase/instance/get-info/get-instance-info.dto";
-import {GetQrCodeUseCaseDTO} from "../usecase/instance/get-qr-code/get-qr-code.usecase.dto";
+import {
+    GetQrCodeUseCaseInputDTO,
+    GetQrCodeUseCaseOutPutDTO
+} from "../usecase/instance/get-qr-code/get-qr-code.usecase.dto";
 import {DeleteInstanceUseCaseDto} from "../usecase/instance/delete/delete-instance.usecase.dto";
 import {SendTextMessageUseCaseDto} from "../usecase/message/send-text-message/send-text-message.dto";
 import {SendUrlMediaFileUseCaseDto} from "../usecase/message/send-url-media-file/send-url-media-file.usecase.dto";
+import {InitInstanceDto} from "../usecase/instance/init/init-instance.dto";
 
 interface InstanceService {
     init(input: InitInstanceDto): Promise<void>;
     info(input: GetInfoUseCaseDTO): Promise<void>;
-    qr(input: GetQrCodeUseCaseDTO): Promise<void>;
+    qr(input: GetQrCodeUseCaseInputDTO): Promise<GetQrCodeUseCaseOutPutDTO>;
     logout(input: LogoutInstanceUseCaseDto): Promise<void>;
     delete(input: DeleteInstanceUseCaseDto): Promise<void>;
 }
