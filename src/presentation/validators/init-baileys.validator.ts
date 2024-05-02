8092b14e3a9ee4941aff9afafa2e9a288e9bd7ed
yury-tomaz <contato@viladev.com>
2024-05-02 20:41:19 -0300

    refactor: initBaileysValidator setting

--- src/presentation/validators/init-baileys.validator.ts
+++ src/presentation/validators/init-baileys.validator.ts
@@ -1,11 +1,6 @@
 import * as yup from "yup";
 
 export const initBaileysValidator = yup.object().shape({
-    key: yup.string().required(),
-    webhook: yup.string().url(),
-    allowWebhook: yup.boolean(),
-    heardEvents: yup.array().of(yup.string()),
-    isWebhookBase64: yup.boolean(),
-    markMessagesRead: yup.boolean(),
-    apiKey: yup.string()
+    name: yup.string().required(),
+    belongsTo: yup.string().required()
 });
