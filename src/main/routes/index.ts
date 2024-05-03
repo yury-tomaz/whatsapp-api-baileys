a3282a54898535fc7321c8098bb707d6a3a95b06
yury-tomaz <contato@viladev.com>
2024-05-02 22:57:18 -0300

    refactor: adjust route pattern

--- src/main/routes/index.ts
+++ src/main/routes/index.ts
@@ -6,7 +6,7 @@ import messageRoute from "./message.route";
 import instanceRoute from "./instance.route";
 const router = Router();
 
-router.use('/api/whatsapp/instances', instanceRoute);
+router.use(instanceRoute);
 router.use('/api/whatsapp/message', messageRoute);
 router.use('/api/whatsapp/group', groupRoute);
 router.use('/api/whatsapp/misc', miscRoute );
