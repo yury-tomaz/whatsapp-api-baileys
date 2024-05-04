1ab66acf375eb89d552dbb22d9e70eb579c9a7ef
yury-tomaz <contato@viladev.com>
2024-05-04 13:44:27 -0300

    refactor: Refactor access to 'init' property

--- src/presentation/controllers/baileys-controllers/index.ts
+++ src/presentation/controllers/baileys-controllers/index.ts
@@ -1,58 +1,23 @@
-import {ControllerInterface} from "../../interfaces/controller.interface";
-import {HttpRequest} from "../../http-types/http-request";
-
-interface ControllersProps{
-    init: ControllerInterface;
-    // info: ControllerInterface;
-    // qr: ControllerInterface;
-    // logout: ControllerInterface;
-    // delete: ControllerInterface;
-    // sendTextMessage: ControllerInterface;
-    // sendUrlMediaFile: ControllerInterface;
-    // sendMediaFile: ControllerInterface;
-}
+import { ControllerInterface } from "../../interfaces/controller.interface";
 
 export class BaileysControllerFacade {
-    private readonly _init: ControllerInterface;
-    // private readonly _info: ControllerInterface;
-    // private readonly _qr: ControllerInterface;
-    // private readonly _logout: ControllerInterface;
-    // private readonly _delete: ControllerInterface;
-    // private readonly _sendTextMessage: ControllerInterface;
-    // private readonly _sendUrlMediaFile: ControllerInterface;
-    // private readonly _sendMediaFile: ControllerInterface;
-    constructor(props: ControllersProps) {
-        this._init = props.init;
-        // this._info = props.info;
-        // this._qr = props.qr;
-        // this._logout = props.logout;
-        // this._delete = props.delete;
-        // this._sendTextMessage = props.sendTextMessage;
-        // this._sendUrlMediaFile = props.sendUrlMediaFile;
-        // this._sendMediaFile = props.sendMediaFile;
-    }
-    init(request: HttpRequest){
-        return this._init.handle(request)
-    }
-    // info(request: HttpRequest){
-    //     return this._info.handle(request)
-    // }
-    // qr(request: HttpRequest){
-    //     return this._qr.handle(request)
-    // }
-    // logout(request: HttpRequest){
-    //     return this._logout.handle(request)
-    // }
-    // delete(request: HttpRequest){
-    //     return this._delete.handle(request)
-    // }
-    // sendTextMessage(request: HttpRequest){
-    //     return this._sendTextMessage.handle(request)
-    // }
-    // sendUrlMediaFile(request: HttpRequest){
-    //     return this._sendUrlMediaFile.handle(request)
-    // }
-    // sendMediaFile(request: HttpRequest){
-    //     return this._sendMediaFile.handle(request)
-    // }
+    constructor(private props: {
+        init: ControllerInterface;
+        // info: ControllerInterface;
+        // qr: ControllerInterface;
+        // logout: ControllerInterface;
+        // delete: ControllerInterface;
+        // sendTextMessage: ControllerInterface;
+        // sendUrlMediaFile: ControllerInterface;
+        // sendMediaFile: ControllerInterface;
+    }) {}
+
+    get init() { return this.props.init; }
+    // get info() { return this.props.info; }
+    // get qr() { return this.props.qr; }
+    // get logout() { return this.props.logout; }
+    // get delete() { return this.props.delete; }
+    // get sendTextMessage() { return this.props.sendTextMessage; }
+    // get sendUrlMediaFile() { return this.props.sendUrlMediaFile; }
+    // get sendMediaFile() { return this.props.sendMediaFile; }
 }
