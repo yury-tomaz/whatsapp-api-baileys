e3f299c063f31e43a6f80c5f048d5d3b70ad65d5
yury-tomaz <contato@viladev.com>
2024-05-04 13:49:59 -0300

    test: Add Integration Tests for Baileys Instance Initialization

--- test/e2e/init-baileys-instance.e2e-spec.ts
+++ test/e2e/init-baileys-instance.e2e-spec.ts
@@ -1,23 +1,26 @@
 import request from 'supertest';
-import { IncomingMessage, Server, ServerResponse } from 'http';
-import {app, mongoClient} from '../../src/main/server/app'
-import { faker } from '@faker-js/faker';
+import {IncomingMessage, Server, ServerResponse} from 'http';
+import {app} from '../../src/main/server/app'
+import {faker} from '@faker-js/faker';
 import TestAgent from 'supertest/lib/agent';
 import {logger} from "../../src/modules/@shared/infra/logger";
+import {dbConnect, dbDisconnect} from "../../src/modules/@shared/infra/persistence/settings/connection";
 
 describe('Baileys instance ini', () => {
     let server: Server<typeof IncomingMessage, typeof ServerResponse>;
     let api: TestAgent;
 
     beforeAll(async () => {
+        await dbConnect()
         server = app.listen(() => logger.info('Server is running on port'));
         api = request(server);
     });
 
     afterAll(async () => {
-        await mongoClient.close()
+        await dbDisconnect()
         server.close();
     });
+
 
     it('should return 200 when POST /instance', async () => {
         const body = {
@@ -26,7 +29,7 @@ describe('Baileys instance ini', () => {
         }
 
         const header = {
-            'Authorization' : `Bearer ${process.env.TOKEN}`
+            'Authorization': `Bearer ${process.env.TOKEN}`
         }
 
         const response = await api.post('/instance')
@@ -34,6 +37,23 @@ describe('Baileys instance ini', () => {
             .send(body)
 
         expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('message')
+    });
+
+    it('should return 400 when POST /instance with missing name', async () => {
+        const body = {
+            belongsTo: faker.string.uuid()
+        }
+
+        const header = {
+            'Authorization': `Bearer ${process.env.TOKEN}`
+        }
+
+        const response = await api.post('/instance')
+            .set(header)
+            .send(body)
+
+        expect(response.status).toBe(400);
     });
 
 });
