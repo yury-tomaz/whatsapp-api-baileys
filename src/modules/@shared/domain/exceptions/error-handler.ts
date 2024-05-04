4fb9b988f1aa0441af8c8eee06873492d3038768
yury-tomaz <contato@viladev.com>
2024-05-04 13:41:49 -0300

    feat: implement handleYupError method
    implemented this method to handle yup error type

--- src/modules/@shared/domain/exceptions/error-handler.ts
+++ src/modules/@shared/domain/exceptions/error-handler.ts
@@ -1,39 +1,54 @@
-import { Response } from "express";
-import { AppError, HttpCode } from "./app-error";
+import {Response} from "express";
+import {AppError, HttpCode} from "./app-error";
+import {ValidationError} from "yup";
+import {logger} from "../../infra/logger";
 
 class ErrorHandler {
-  private isTrustedError(error: Error): boolean {
-    if (error instanceof AppError) {
-      return error.isOperational;
+    private isTrustedError(error: Error): boolean {
+
+        if (error instanceof AppError) {
+            return error.isOperational;
+        }
+        return false;
     }
-    return false;
-  }
 
-  private handleTrustedError(error: AppError, response: Response): void {
-    response.status(error.statusCode).json({ 
-      message: error.message,
-      code: error.statusCode,
-    });
-  }
-
-
-  private handleCriticalError(error: Error | AppError, response?: Response): void {
-    if (response) {
-      response
-        .status(HttpCode.INTERNAL_SERVER_ERROR)
-        .json({ message: 'Internal server error' });
+    private handleTrustedError(error: AppError, response: Response): void {
+        response.status(error.statusCode).json({
+            message: error.message,
+            code: error.statusCode,
+        });
     }
-    process.exit(1);
-  }
 
-  public handleError(error: Error | AppError, response?: Response): void {
-    
-    if (this.isTrustedError(error) && response) {
-      this.handleTrustedError(error as AppError, response);
-    } else {
-      this.handleCriticalError(error, response);
+
+    private handleCriticalError(error: Error | AppError, response?: Response): void {
+        if (response) {
+            response
+                .status(HttpCode.INTERNAL_SERVER_ERROR)
+                .json({message: 'Internal server error'});
+        }
+        process.exit(1);
     }
-  }
+
+    private handleYupError(error: ValidationError, response: Response) {
+        response.status(HttpCode['BAD_REQUEST']).json({
+            message: error,
+            code: HttpCode['BAD_REQUEST']
+        })
+    }
+
+    public handleError(error: Error | AppError | ValidationError, response?: Response): void {
+        logger.error(error);
+
+        if (error instanceof ValidationError && response) {
+            this.handleYupError(error as ValidationError, response)
+        }
+
+        if (this.isTrustedError(error) && response) {
+            this.handleTrustedError(error as AppError, response);
+        } else {
+            this.handleCriticalError(error, response);
+        }
+    }
 }
 
 export const errorHandler = new ErrorHandler();
