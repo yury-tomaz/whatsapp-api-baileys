686f352e161a6a96a97fece5f60054552569f2e2
yury-tomaz <contato@viladev.com>
2024-05-06 22:30:32 -0300

    feat: implement route `/instance/:id/qr`

--- src/main/routes/instance.route.ts
+++ src/main/routes/instance.route.ts
@@ -7,6 +7,12 @@ const controller = BaileysComposer.create()
 
 instanceRoute.post(
     '/instance',
-    (req: Request, res: Response) =>  requestAdapter(req, res, controller.init) )
+    (req: Request, res: Response) =>  requestAdapter(req, res, controller.init)
+)
+
+instanceRoute.get(
+    '/instance/:id/qr',
+    (req: Request, res: Response) =>  requestAdapter(req, res, controller.qr)
+)
 
 export default instanceRoute;
