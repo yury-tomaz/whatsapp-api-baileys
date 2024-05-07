739cea341ed000e6f9f445dfb0d443998037bf87
yury-tomaz <contato@viladev.com>
2024-05-06 22:25:57 -0300

    feat: implement qr in BaileysComposer class

--- src/main/compose/baileys/bailyes.composer.ts
+++ src/main/compose/baileys/bailyes.composer.ts
@@ -3,14 +3,19 @@ import {
     InitBaileysController
 } from "../../../presentation/controllers/baileys-controllers/instance/init-baileys.controller";
 import {BaileysFactory} from "../../../modules/baileys/factory/baileysFactory";
+import {
+    GetQrCodeController
+} from "../../../presentation/controllers/baileys-controllers/instance/get-qr-code.controller";
 
 export class BaileysComposer{
     private static baileysFactory = BaileysFactory.create()
     static create(){
-        const init = new InitBaileysController(this.baileysFactory)
+        const init = new InitBaileysController(this.baileysFactory);
+        const qr = new GetQrCodeController(this.baileysFactory);
 
         return new BaileysControllerFacade({
-            init
+            init,
+            qr
         })
     }
 }
