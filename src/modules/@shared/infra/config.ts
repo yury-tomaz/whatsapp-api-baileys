290896231de0301e7f586e1e537243e4ebcad60b
yury-tomaz <contato@viladev.com>
2024-05-11 17:53:11 -0300

    feat: implement Config class
    Implemented this class to handle environment variables.

---
+++ src/modules/@shared/infra/config.ts
@@ -0,0 +1,41 @@
+import { config as readEnv } from 'dotenv';
+import { join } from 'path';
+export class Config {
+  static env: any = null;
+
+  static port(){
+    Config.readEnv();
+
+    return Config.env.PORT
+  }
+  static rabbitmqUri(){
+    Config.readEnv();
+
+    return Config.env.RABBITMQ_URI
+  }
+
+  static db(){
+    Config.readEnv();
+
+    return{
+      uri: Config.env.MONGO_URI,
+      dbName: Config.env.MONGO_DB_NAME,
+      colection: Config.env.MONGO_DB_COLLECTION
+    }
+  }
+
+  static readEnv() {
+    if (Config.env) {
+      return;
+    }
+
+    const { parsed } = readEnv({
+      path: join(__dirname, `../../../../envs/.env.${process.env.NODE_ENV}`),
+    });
+
+    Config.env = {
+      ...parsed,
+      ...process.env,
+    };
+  }
+}
