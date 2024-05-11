4c0c8a929f709d09201b3c8efe4409971363bdef
Gabriel Freitas <70125057+Gabriel-souzaa@users.noreply.github.com>
2024-05-11 18:35:18 -0300

    fix: to adjust validators (#70)

--- src/presentation/validators/baileys/misc/get-profile-picture.validator.ts
+++ src/presentation/validators/baileys/misc/get-profile-picture.validator.ts
@@ -3,5 +3,4 @@ import * as yup from "yup";
 export const getProfilePictureValidator = yup.object().shape({
   id: yup.string().required(),
   to: yup.string().required(),
-  action: yup.string().required(),
 })
