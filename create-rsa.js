a4790c1ce780867f13564bf5d45d7e28684d047a
yury-tomaz <contato@viladev.com>
2024-05-11 20:32:30 -0300

    feat: implement updateEnvFile function

--- create-rsa.js
+++ create-rsa.js
@@ -1,4 +1,29 @@
+const fs = require('fs');
 const crypto = require('crypto');
+
+function updateEnvFile(privateKey, publicKey) {
+  const envFilePath = __dirname + '/envs/.env.development';
+
+  if (!fs.existsSync(envFilePath)) {
+    console.error('.env.development file not found.');
+    return;
+  }
+
+  let envFileContent = fs.readFileSync(envFilePath, 'utf-8');
+
+  if (envFileContent.includes('JWT_PRIVATE_KEY=') || envFileContent.includes('JWT_PUBLIC_KEY=')) {
+    console.error('Keys already exist in the .env.development file.');
+    return;
+  }
+
+  envFileContent += `\nPRIVATE_KEY="${privateKey.replace(/\n/g, '\\n')}"`;
+  envFileContent += `\nPUBLIC_KEY="${publicKey.replace(/\n/g, '\\n')}"`;
+
+  fs.writeFileSync(envFilePath, envFileContent);
+
+  console.log('Updated keys in the .env.development file successfully.');
+}
+
 
 const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
   modulusLength: 2048,
@@ -12,8 +37,4 @@ const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
   },
 });
 
-const privateKeyInline = privateKey.replace(/\n/g, '\\n');
-console.log(privateKeyInline);
-
-const publicKeyInline = publicKey.replace(/\n/g, '\\n');
-console.log(publicKeyInline);
+updateEnvFile(privateKey, publicKey);
