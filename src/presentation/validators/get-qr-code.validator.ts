2eddd346a47462ad771e69b0174ec960caa60790
yury-tomaz <contato@viladev.com>
2024-05-13 22:56:02 -0300

    fix: define belongsTo optional

--- src/presentation/validators/get-qr-code.validator.ts
+++ src/presentation/validators/get-qr-code.validator.ts
@@ -2,5 +2,5 @@ import * as yup from 'yup';
 
 export const getQrCodeValidator = yup.object().shape({
   id: yup.string().required(),
-  belongsTo: yup.string().required(),
+  belongsTo: yup.string().optional(),
 });
