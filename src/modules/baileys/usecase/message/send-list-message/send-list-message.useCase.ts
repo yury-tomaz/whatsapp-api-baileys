00d745b8734fb52020ce79b61601307e3ee46e9f
Gabriel-souzaa <gabrielfreitassouzaa@gmail.com>
2024-05-15 00:07:04 -0300

    feat: to add usecase list message

---
+++ src/modules/baileys/usecase/message/send-list-message/send-list-message.useCase.ts
@@ -0,0 +1,26 @@
+import { BaileysInstanceRepositoryInMemory } from '../../../repository/baileys-instance-repository-in-memory';
+import { getWhatsAppId } from '../../../helpers/get-whats-app-Id';
+import { checkInstance } from '../../../helpers/check-Instance';
+import { SendListMessageUseCaseDto } from './send-list-message.dto';
+
+export class SendListMessageUseCase {
+  constructor(private baileysManager: BaileysInstanceRepositoryInMemory) {}
+
+  async execute(input: SendListMessageUseCaseDto) {
+    const result = await checkInstance(input.id, this.baileysManager);
+
+    const sock = result.waSocket!;
+
+    const whatsappId = getWhatsAppId(input.to);
+    await result.verifyId(whatsappId);
+
+    await sock.sendMessage(whatsappId, {
+      text: input.text,
+      buttonText: input.buttonText,
+      footer: input.description,
+      title: input.title,
+      sections: input.sections,
+      viewOnce: true,
+    });
+  }
+}
