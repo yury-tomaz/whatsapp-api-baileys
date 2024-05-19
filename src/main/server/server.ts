95eb7613eb2455c985bdbfa63a4d1b860da28289
Yury Tomaz <contato@viladev.com>
2024-05-19 01:17:27 -0300

    refactor: Improve the way you handle connecting to Mongo DB

--- src/main/server/server.ts
+++ src/main/server/server.ts
@@ -1,10 +1,11 @@
 import { logger } from '../../modules/@shared/infra/logger';
 import { app } from './app';
-import { dbConnect } from '../../modules/@shared/infra/persistence/settings/connection';
+
 import { Config } from '../../modules/@shared/infra/config';
+import { mongoDBManager } from '../../modules/@shared/infra/persistence/settings/connection';
 
 const port = Config.port();
 app.listen(Config.port(), async () => {
-  await dbConnect();
+  await mongoDBManager.connect();
   logger.info(`Server is running on port ${port}`);
 });
