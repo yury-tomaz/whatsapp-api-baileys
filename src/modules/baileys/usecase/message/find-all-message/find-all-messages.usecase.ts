065e2df033e1ceadfff2286a98a7cde2dc81f1a6
Gabriel-souzaa <gabrielfreitassouzaa@gmail.com>
2024-05-21 02:06:17 -0300

    fix: to adjust usecase find messages

--- src/modules/baileys/usecase/message/find-all-message/find-all-messages.usecase.ts
+++ src/modules/baileys/usecase/message/find-all-message/find-all-messages.usecase.ts
@@ -1,15 +1,18 @@
 import { FindAllMessageUseCaseDto } from './find-all-message.dto';
 import { MessageRepository } from '../../../repository/message-repository';
+import { getWhatsAppId } from '../../../helpers/get-whats-app-Id';
 
 export class FindAllMessageUseCase {
   constructor(private messageRepository: MessageRepository) {}
 
   async execute(input: FindAllMessageUseCaseDto) {
+    const whatsappId = getWhatsAppId(input.to);
+
     const messages = await this.messageRepository.find({
       sessionId: input.id,
       page: Number(input.page),
       limit: Number(input.limit),
-      to: input.to,
+      to: whatsappId,
     });
 
     return messages;
